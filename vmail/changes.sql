78d281cdd769caae1eb1d4c5f8a974da445baf60
Radu Rendec <radu.rendec@mindbit.ro>
2010-07-08 18:35:28 +0300

    Add global domain forwards tables
    Signed-off-by: Radu Rendec <radu.rendec@mindbit.ro>

--- vmail/changes.sql
+++ vmail/changes.sql
@@ -3,3 +3,8 @@ alter table mailbox_forwards add foreign key (mailbox_id) references mailboxes(m
 
 alter table mailboxes add column copy_on_forward boolean not null default false;
 
+
+create table global_mail_aliases(global_mail_alias_id serial not null primary key, domain_id int not null, name varchar not null);
+alter table global_mail_aliases add foreign key(domain_id) references domains(domain_id);
+create table global_mail_alias_to(global_mail_alias_to_id serial not null primary key, global_mail_alias_id int not null, address varchar not null);
+alter table global_mail_alias_to add foreign key(global_mail_alias_id) references global_mail_aliases(global_mail_alias_id);
