a3b3b3db9d1c5b2e114c76401beced4e8725fa4d
Radu Rendec <radu.rendec@mindbit.ro>
2010-08-13 15:12:21 +0300

    Move backend/model outside web tree
    Signed-off-by: Radu Rendec <radu.rendec@mindbit.ro>

--- web/common.php
+++ web/common.php
@@ -5,8 +5,8 @@ ENV::setup();
 $path = dirname($_SERVER["SCRIPT_FILENAME"]);
 
 // propel init
-set_include_path($path . "/propel/build/classes" . PATH_SEPARATOR . get_include_path());
+set_include_path($path . "/../backend/model/build/classes" . PATH_SEPARATOR . get_include_path());
 require_once ('propel/Propel.php');
-Propel::init($path . "/propel/build/conf/mipanel-conf.php");
+Propel::init($path . "/../backend/model/build/conf/mipanel-conf.php");
 // end of propel init
 ?>
