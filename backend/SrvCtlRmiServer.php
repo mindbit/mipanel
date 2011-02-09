b2ff0d15ea3038b21410de44ee485c05f5cdf9f1
Ionut Nicu <ionut.nicu@mindbit.ro>
2011-02-09 12:03:06 +0200

    Add logging setup in rmi server
    However, this doesn't seem to work yet ...
    
    Signed-off-by: Ionut Nicu <ionut.nicu@mindbit.ro>

--- backend/SrvCtlRmiServer.php
+++ backend/SrvCtlRmiServer.php
@@ -1,6 +1,8 @@
 <?
 require_once("Env.php");
+require_once "Log.php";
 Env::setup();
+Env::setLogger(Log::factory('syslog', LOG_LOCAL5, 'mipanel'));
 
 require_once("Rmi.php");
 require_once("SrvCtl.php");
