d27a6750c62cd416b8130ec67285a4f9c24ae5d5
Daniela Rendec <daniela.rendec@mindbit.ro>
2017-05-20 15:42:23 +0100

    Add autoloader and propel support
    Initialise MPL using the new API based on composer and autoloader.
    
    Signed-off-by: Daniela Rendec <daniela.rendec@mindbit.ro>

--- web/common.php
+++ web/common.php
@@ -1,18 +1,15 @@
-<?
-require_once "Env.php";
-require_once "ThrowErrorHandler.php";
-require_once "Log.php";
+<?php
 
-Env::setup();
-$logger = Log::factory('syslog', LOG_LOCAL5, 'mipanel');
-$logger->setPriority(PEAR_LOG_WARNING);
-Env::setLogger($logger);
+use Mindbit\Mpl\MPL;
+use Mindbit\Mpl\Logging\SyslogLogger;
 
-$path = dirname($_SERVER["SCRIPT_FILENAME"]);
+// Initialise autoloader
+require __DIR__ . '/../vendor/autoload.php';
 
-// propel init
-set_include_path($path . "/../backend/model/build/classes" . PATH_SEPARATOR . get_include_path());
-require_once ('propel/Propel.php');
-Propel::init($path . "/../backend/model/build/conf/mipanel-conf.php");
-// end of propel init
-?>
+// Initialise MPL
+MPL::init();
+$log = new SyslogLogger("mipanel");
+MPL::setLogger($log);
+
+// Initialise Propel
+require __DIR__ . '/../model/generated-conf/config.php';
