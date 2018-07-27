4e33b8849fcc59d5c5925b2797448ed6029ac34b
Radu Rendec <radu.rendec@mindbit.ro>
2018-07-26 21:44:27 -0400

    Update authentication to match latest mpl
    Signed-off-by: Radu Rendec <radu.rendec@mindbit.ro>

--- view/AuthResponse.php
+++ view/AuthResponse.php
@@ -7,7 +7,7 @@ use Mindbit\Mpl\Template\Template;
 
 class AuthResponse extends HtmlResponse
 {
-    const TEMPLATE_BODY = 'mindbit.mipanel.authresponse.html';
+    const TEMPLATE_BODY = 'application.authresponse.html';
 
     public function send()
     {
@@ -15,7 +15,7 @@ class AuthResponse extends HtmlResponse
         $this->addCssRef('css/style.css');
         $this->template->replaceBlock(self::BLOCK_BODY_INNER, Template::load(self::TEMPLATE_BODY));
         $this->template->setVariable('username', @$_REQUEST['username']);
-        $this->template->setVariable('action', BaseAuthRequest::ACTION_LOGIN);
+        $this->template->setVariable('auth', BaseAuthRequest::ACTION_LOGIN);
 
         if ($this->request->getStatus() == BaseAuthRequest::STATUS_FAILED) {
             $this->template->getBlock('auth.failed')->show();
